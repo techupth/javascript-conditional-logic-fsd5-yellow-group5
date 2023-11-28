163370cc66fadfc9b528f6992239d9aaa138cae0
ณัฐพร นาคพงษ์ <Missterdonat33@gmail.com>
2023-11-28 19:25:29 +0700

    EX:Conditional Logic

--- ex-4.js
+++ ex-4.js
@@ -1,5 +1,15 @@
 //Exercise 4
 // Switch Statement
 let lightBulbStatus = "On";
-
 //Start coding here
+switch (lightBulbStatus) {
+    case "On":
+    console.log("Light bulb is On.")
+    break;
+    case "Broken":
+    console.log("Light bulb is Broken.")
+    break;
+    case "Off":
+    console.log("Light bulb is Off.")
+    break;
+}
--- ex-7.js
+++ ex-7.js
@@ -1,6 +1,8 @@
 //Start coding here
-const greeting = "Hello ! 😁";
+function greeting () {
+    const greeting = "Hello ! 😁";
+    console.log (greeting)
+};
 
 greeting();
-
 console.log("This line will be executed without any error.");
